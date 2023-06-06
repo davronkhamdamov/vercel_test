d252fedac162f508ea96f76ce802b4ecc507be54
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-06 23:09:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send({
+        message: "it's working"
+    })
+})
+
+app.listen(3000)
